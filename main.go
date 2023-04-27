30291748a8e56f11f493a74091c813566764158a
daryn <daryn200150@gmail.com>
2023-04-27 10:47:42 +0600

    added Course and Student Struct

--- main.go
+++ main.go
@@ -1,46 +1,137 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/redis/go-redis/v9"
-	"log"
-	"net/http"
-	"os"
 )
 
 var client *redis.Client
 
-func main() {
-	fmt.Println("BABYMETAL TOP")
+var ctx = context.Background()
 
-	redisAddr := os.Getenv("REDIS_ADDR")
-	if redisAddr == "" {
-		redisAddr = "localhost:6379"
-	}
-
+func initDb() {
 	client = redis.NewClient(&redis.Options{
-		Addr:     redisAddr,
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+}
+
+func main() {
+	initDb()
+
+	var grades1 = make(map[string]int)
+	var grades2 = make(map[string]int)
+
+	grades1["123"] = 1
+	grades1["456"] = 2
+
+	grades2["555"] = 1
+	grades2["666"] = 2
+
+	var course1 = Course{"321", grades1}
+	var course2 = Course{"flGTnyq", grades2}
+
+	var s = Student{"Ivan", "123", []Course{course1}}
+	var s2 = Student{"Empty", "1234", []Course{course2}}
+	s.AddCourse(course1)
+	s.ViewCourses(s.ID)
+
+	s2.AddCourse(course2)
+	s2.ViewCourses(s2.ID)
+
+}
+
+type Course struct {
+	Code          string
+	StudentsGrade map[string]int
+}
+
+func (c *Course) initCourseDb() {
+	client = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
 		Password: "",
 		DB:       0,
 	})
 
-	http.HandleFunc("/", handleRequest)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	err := client.Set(ctx, c.Code, c.StudentsGrade, 0).Err()
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (c *Course) getCourseCode() {
 
 }
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	count, err := client.Incr(r.Context(), "counter").Result()
+type Student struct {
+	Name    string
+	ID      string
+	Courses []Course
+}
+
+func (s *Student) AddCourse(c Course) {
+	s.Courses = append(s.Courses, c)
+	val, err := json.Marshal(c)
 
 	if err != nil {
-		http.Error(w, "Error fetching counter", http.StatusInternalServerError)
-		return
+		panic(err)
 	}
 
-	var htmlText string = "<body>\n" +
-		"<h1 style=\"color: red; align: center; \" >BABYMETAL TOP</h1>\n" +
-		"<script>alert('BABYMETAL TOP')</script>" +
-		"</body>"
+	clientErr := client.Set(ctx, s.ID, val, 0).Err()
+	if clientErr != nil {
+		panic(clientErr)
+	}
 
-	fmt.Fprintf(w, htmlText+"\nThis page has been viewed %d times!", count)
+}
+
+func (s *Student) UpdateCourse(c Course) {
+	val, err := json.Marshal(c)
+
+	if err != nil {
+		panic(err)
+	}
+
+	client.Set(ctx, s.ID, val, 0)
+
+	for j := 0; j < len(s.Courses); j++ {
+		if s.Courses[j].Code == c.Code {
+			s.Courses[j] = c
+		}
+	}
+}
+
+func (s *Student) DeleteCourse(c Course) { // todo use redis
+	for j := 0; j < len(s.Courses); j++ {
+		if s.Courses[j].Code == c.Code {
+			s.Courses = append(s.Courses[:j], s.Courses[j+1:]...)
+		}
+	}
+}
+
+func (s *Student) ViewCourses(id string) {
+	courses, err := client.Get(ctx, id).Result()
+
+	val := json.Unmarshal([]byte(courses), &Course{})
+
+	fmt.Println("Course: ", val)
+
+	if err != nil {
+		panic(err)
+	} else if err == redis.Nil {
+		fmt.Println("student does not exist")
+	} else {
+		fmt.Println("courses: ", courses)
+		fmt.Println("student: ", s)
+		for key, course := range courses {
+			fmt.Println("key: ", key, "course: ", course)
+
+			if string(key) == s.ID {
+				fmt.Println("student Id: ", id, " course: ", course)
+			}
+		}
+	}
 }
